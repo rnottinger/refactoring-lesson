0215ed151ec033f1c05ae4e06183aec55d97fb19
Richard Ottinger <richard.ottinger@gmail.com>
2021-10-01 15:35:14 -0500

    tests are passing.  Good Refactor! rename Parameters in the extracted code to make them clearer..$perf => $aPerformance

--- src/Theatrical/statement.php
+++ src/Theatrical/statement.php
@@ -38,22 +38,22 @@ function statement ($invoice, $plays) : string
 
 // check variable scope: perf, play, and thisAmount.
 
-function amountFor($perf, $play)
+function amountFor($aPerformance, $play)
 {
     $result = 0;
     switch ($play["type"]) {
         case "tragedy":
             $result = 40000;
-            if ($perf->audience > 30) {
-                $result += 1000 * ($perf->audience - 30);
+            if ($aPerformance->audience > 30) {
+                $result += 1000 * ($aPerformance->audience - 30);
             }
             break;
         case "comedy":
             $result = 30000;
-            if ($perf->audience > 20) {
-                $result += 10000 + 500 * ($perf->audience - 20);
+            if ($aPerformance->audience > 20) {
+                $result += 10000 + 500 * ($aPerformance->audience - 20);
             }
-            $result += 300 * $perf->audience;
+            $result += 300 * $aPerformance->audience;
             break;
         default:
             throw new \Exception("unknown play type: {$play["type"]}");
