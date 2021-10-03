22bdcca7a04f2765a677c6be48ba6115a3bb0c9b
Richard Ottinger <richard.ottinger@gmail.com>
2021-10-02 21:12:00 -0500

    tests pass! Good Refactor!
    write a test it_generates_a_html_statement() to check the an HTML

--- tests/Unit/StatementTest.php
+++ tests/Unit/StatementTest.php
@@ -11,7 +11,7 @@ include __DIR__ . '/../../src/Theatrical/statement.php';
 class StatementTest extends TestCase
 {
     /** @test */
-    public function it_generates_a_statement()
+    public function it_generates_a_plain_text_statement()
     {
         // Setup
         $playsJson = '{"hamlet": {"name": "Hamlet", "type": "tragedy"},"as-like": {"name": "As You Like It", "type": "comedy"},"othello": {"name": "Othello", "type": "tragedy"}}';
@@ -31,6 +31,28 @@ class StatementTest extends TestCase
 
     }
 
+
+    /** @test */
+    public function it_generates_a_html_statement()
+    {
+        // Setup
+        $playsJson = '{"hamlet": {"name": "Hamlet", "type": "tragedy"},"as-like": {"name": "As You Like It", "type": "comedy"},"othello": {"name": "Othello", "type": "tragedy"}}';
+        $invoiceJson = '[{"customer": "BigCo","performances": [{"playID": "hamlet","audience": 55},{"playID": "as-like","audience": 35},{"playID": "othello","audience": 40}]}]';
+
+        $plays = json_decode($playsJson,true);
+        $invoice = json_decode($invoiceJson);
+
+
+//        $expected = "Statement for BigCo\n  Hamlet: 650.00 (55 seats)\n  As You Like It: 580.00 (35 seats)\n  Othello: 500.00 (40 seats)\nAmount owed is 1,730.00\nYou earned 47 credits\n";
+        $expected = "<h1>Statement for BigCo</h1>\n<table>\n<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>650.00</td></tr>\n  <tr><td>As You Like It</td><td>35</td><td>580.00</td></tr>\n  <tr><td>Othello</td><td>40</td><td>500.00</td></tr>\n</table>\n<p>Amount owed is <em>1,730.00</em></p>\n<p>You earned <em>47</em> credits</p>\n";
+        // Act
+        $actual = Theatrical\htmlStatement($invoice, $plays);
+
+
+        // Assert
+        $this->assertSame($expected,$actual,'The statement printed does not match the expected statement');
+
+    }
 
     /** @test */
     public function the_play_type_is_unknown()
