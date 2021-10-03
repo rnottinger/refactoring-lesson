304c94ecba7ce7a97784614a43954c305bfe5268
Richard Ottinger <richard.ottinger@gmail.com>
2021-10-03 12:21:12 -0500

    tests pass! Good Refactor!
    replace the constructor call with a function
    So I use Replace Constructor with Factory Function

--- src/Theatrical/PerformanceCalculator.php
+++ src/Theatrical/PerformanceCalculator.php
@@ -16,28 +16,21 @@ class PerformanceCalculator
     public function amount()
     {
         $result = 0;
-//    switch ($aPerformance->play["type"]) {
         switch ($this->play["type"]) {
             case "tragedy":
                 $result = 40000;
-//                if ($aPerformance->audience > 30) {
                 if ($this->performance->audience > 30) {
-//                    $result += 1000 * ($aPerformance->audience - 30);
                     $result += 1000 * ($this->performance->audience - 30);
                 }
                 break;
             case "comedy":
                 $result = 30000;
-//                if ($aPerformance->audience > 20) {
                 if ($this->performance->audience > 20) {
-//                    $result += 10000 + 500 * ($aPerformance->audience - 20);
                     $result += 10000 + 500 * ($this->performance->audience - 20);
                 }
-//                $result += 300 * $aPerformance->audience;
                 $result += 300 * $this->performance->audience;
                 break;
             default:
-//                throw new \Exception("unknown play type: " . $aPerformance->play["type"]);
                 throw new \Exception("unknown play type: " . $this->play["type"]);
         }
         return $result;
--- src/Theatrical/createStatementData.php
+++ src/Theatrical/createStatementData.php
@@ -9,13 +9,11 @@ function createStatementData($invoice, $plays): \stdClass
     $data = new \stdClass();
     $data->customer = $invoice[0]->customer;
     $data->performances = array_map(static function ($aPerformance) use ($plays) {
-        $calculator = new PerformanceCalculator($aPerformance, playFor($plays, $aPerformance));
+//        $calculator = new PerformanceCalculator($aPerformance, playFor($plays, $aPerformance));
+        $calculator = createPerformanceCalculator($aPerformance, playFor($plays, $aPerformance));
         $result = clone $aPerformance;
-//        $result->play = playFor($plays, $result);
         $result->play = $calculator->play;
-//        $result->amount = amountFor($plays, $result);
         $result->amount = $calculator->amount();
-//        $result->volumeCredits = volumeCreditsFor($result);
         $result->volumeCredits = $calculator->volumeCredits();
         return $result;
     }, $invoice[0]->performances);
@@ -24,17 +22,10 @@ function createStatementData($invoice, $plays): \stdClass
     return $data;
 }
 
-//function volumeCreditsFor($aPerformance)
-//{
-//    $result = 0;
-//    // add volume credits
-//    $result += max($aPerformance->audience - 30, 0);
-//
-//    // add extra credit for every ten comedy attendees
-//    if ("comedy" === $aPerformance->play["type"]) $result += floor($aPerformance->audience / 5);
-//
-//    return $result;
-//}
+function createPerformanceCalculator($aPerformance, $aPlay): PerformanceCalculator
+{
+    return new PerformanceCalculator($aPerformance, $aPlay);
+}
 
 function playFor($plays, $aPerformance) {
     return $plays[$aPerformance->playID];
@@ -44,33 +35,13 @@ function playFor($plays, $aPerformance) {
  * @throws \Exception
  */
 function amountFor($plays, $aPerformance): int
-{   $calc = new PerformanceCalculator(
+{
+    $calc = new PerformanceCalculator(
         $aPerformance,
         playFor($plays, $aPerformance)
     );
     return $calc->amount();
 
-
-
-//    $result = 0;
-//    switch ($aPerformance->play["type"]) {
-//        case "tragedy":
-//            $result = 40000;
-//            if ($aPerformance->audience > 30) {
-//                $result += 1000 * ($aPerformance->audience - 30);
-//            }
-//            break;
-//        case "comedy":
-//            $result = 30000;
-//            if ($aPerformance->audience > 20) {
-//                $result += 10000 + 500 * ($aPerformance->audience - 20);
-//            }
-//            $result += 300 * $aPerformance->audience;
-//            break;
-//        default:
-//            throw new \Exception("unknown play type: " . $aPerformance->play["type"]);
-//    }
-//    return $result;
 }
 
 function totalAmount($data): int
